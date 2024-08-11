e1b9b55b8a928b08438f7be61777afa04ade6462
gabezy <gabrielmoreira2705@gmail.com>
2024-08-11 16:33:52 -0300

    eureka server created

---
+++ eureka-serve/src/test/java/com/gabezy/eurekaserve/EurekaServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabezy.eurekaserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
